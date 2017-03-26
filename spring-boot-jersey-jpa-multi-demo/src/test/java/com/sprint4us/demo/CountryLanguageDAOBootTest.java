2289e607f784fc90429b9782864306c1fe930c04
sprint4us <sprint21er@googlemail.com>
2017-03-26 14:06:48 +0200

    complete last commit

--- spring-boot-jersey-jpa-multi-demo/src/test/java/com/sprint4us/demo/CountryLanguageDAOBootTest.java
+++ spring-boot-jersey-jpa-multi-demo/src/test/java/com/sprint4us/demo/CountryLanguageDAOBootTest.java
@@ -1,14 +1,11 @@
 package com.sprint4us.demo;
 
-import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CountryLanguageDAOBootTest extends CountryLanguageDAOTestBase {
 
 }
--- spring-classic-jersey-jpa-multi-demo/src/test/java/com/sprint4us/demo/CountryLanguageDAOClassicTest.java
+++ spring-classic-jersey-jpa-multi-demo/src/test/java/com/sprint4us/demo/CountryLanguageDAOClassicTest.java
@@ -1,15 +1,12 @@
 package com.sprint4us.demo;
 
-import org.junit.FixMethodOrder;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = {
 		"/applicationContext.xml" })
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CountryLanguageDAOClassicTest extends CountryLanguageDAOTestBase {
 
 }
